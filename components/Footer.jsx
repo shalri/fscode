1b42fa997edec3d90d9b7d0270abff3d51465d85
shalri <fsaonlinework777@gmail.com>
2024-04-23 20:02:33 +0800

    Adjusting Footer layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <footer className="py-5 text-right border-t border-gray-fs-100">
+    <footer className="mt-20 py-5 text-right border-t border-gray-fs-100 flex justify-between items-center">
+      <div className="pl-1 opacity-30 m-auto hover:opacity-80 transition-opacity duration-200">
+        <a href="/">
+          <Image src="/fscode32x32.png" width={32} height={32} alt="logo" />
+        </a>
+      </div>
       <span
         id="date"
-        className="text-gray-500 border-r-5  border-r-8 border-yellow-fs pr-1 pt-[5px]"
+        className="text-gray-500 border-r-5  border-r-8 border-yellow-fs pr-1 pt-[6px] pb-[1px]"
       >
         2024
       </span>
