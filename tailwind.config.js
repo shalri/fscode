3adeb2458caf1ddc94be7ef628694757f55384c3
shalri <fsaonlinework777@gmail.com>
2024-04-27 18:20:08 +0800

    Removing backticks from code elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,30 @@ module.exports = {
         // "gray-line": "rgb(168, 168, 168, 0.3)",
         "gray-fs-100": "rgb(226, 183, 20, 0.2)",
       },
+      typography(theme) {
+        return {
+          DEFAULT: {
+            css: {
+              "code::before": {
+                content: "none", // don’t generate the pseudo-element
+                //                content: '""', // this is an alternative: generate pseudo element using an empty string
+              },
+              "code::after": {
+                content: "none",
+              },
+              code: {
+                // color: theme('colors.slate.500'),
+                // backgroundColor: theme('colors.stone.100'),
+                borderRadius: theme("borderRadius.DEFAULT"),
+                paddingLeft: theme("spacing[1.5]"),
+                paddingRight: theme("spacing[1.5]"),
+                paddingTop: theme("spacing.1"),
+                paddingBottom: theme("spacing.1"),
+              },
+            },
+          },
+        };
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
