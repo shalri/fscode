29560d45c154978119fd38f77271d8dd20ff409e
shalri <fsaonlinework777@gmail.com>
2024-04-29 10:30:45 +0800

    adding antialiased class

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${op.className}`}>
+      <body className={`${op.className} antialiased`}>
         <Header />
         {children}
         <Footer />
