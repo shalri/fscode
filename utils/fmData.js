b4890b3a2d30cc32156b99e04628585be7696dd8
shalri <fsaonlinework777@gmail.com>
2024-09-22 12:22:59 +0800

    Update Frontend Mentor challenge

--- utils/fmData.js
+++ utils/fmData.js
@@ -20,6 +20,11 @@ export const fmChallenges = [
     url: "https://shalri.github.io/fm-todo-app/",
   },
   {
+    title: "Insure landing page",
+    level: "Junior",
+    url: "https://fm-insure-landing-page-fscode.vercel.app/",
+  },
+  {
     title: "Base Apparel Coming Soon Page",
     level: "Newbie",
     url: "https://shalri.github.io/fm-base-apparel-coming-soon/",
